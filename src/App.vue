96fefb1a7d3581e7ebec058827bbcf67bb46355b
Motheen <50198896+motheen-b@users.noreply.github.com>
2024-07-19 15:27:07 -0400

    Laid out margin and padding for main content pane, created Header component and added two "items".

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    
+  <div class="app">
+    <div class="main-container">
+      <Header />
+      <!-- <component :is="showActivateComponent()" /> -->
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+
 export default {
-  
+  data() {
+    return {
+      activatePage: 'home'
+    }
+  },
+  components: {
+    Header
+  },
+  methods: {
+    showActivateComponent() {
+      const components = {
+        'home': Home
+      }
+      return components[self.activatePage] || Home
+    }
+  }
 }
 </script>
 
 <style>
-  
+  .main-container {
+    width: 100%;
+    height: 3160px;
+    margin: 0 auto;
+    max-width: 1200px;
+    border: 2px solid black;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="header-container">
+            <div class="header-items">
+                <div class="item" @click="copyText('+1 (604) 555-5555')">
+                    +1 (604) 555-5555
+                </div>
+                <div class="item" @click="copyText('contact@cegroup.ca')">
+                    contact@cegroup.ca
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    async function copyText(text) {
+        await navigator.clipboard.writeText(text);
+        alert('Text copied to clipboard: ' + text);
+    }
+</script>
+
+<style scoped>
+    .header-container {
+        display: flex;
+        justify-content: flex-end;
+        padding: 0 10px;
+        height: 40px;        
+        background-color: #0E0E0E;
+        /* border: 2px solid red; */
+    }
+    .header-items {
+        display: flex;
+        margin: 10px ;
+        align-items: center;
+        /* border: 2px solid white; */
+    }
+    .item {
+        margin: 8px 8px;
+        color: #FFFFFF;
+    }
+    .item:hover {
+        cursor: pointer;
+        text-decoration: underline;
+    }
+</style>
